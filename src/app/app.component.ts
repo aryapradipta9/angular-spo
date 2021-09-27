5d0e904f334774c0b5415fe03fff13de4c82043c
aryapradipta9 <aryapradipta9@gmail.com>
2021-09-27 23:29:35 +0700

    passing data between app and tracks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-spo';
+  selectedSongs: Array<String> = [];
+  selectEvent(uri: string) {
+    if (this.selectedSongs.includes(uri)) {
+      this.selectedSongs = this.selectedSongs.filter((el) => el !== uri);
+    } else {
+      this.selectedSongs = this.selectedSongs.concat([uri]);
+    }
+  }
 }
