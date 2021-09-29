89734829c742637cd2111562762c279f4153cd36
aryapradipta9 <aryapradipta9@gmail.com>
2021-09-29 22:09:46 +0700

    pass envs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import data from './all-sample';
 
 @Component({
@@ -10,6 +11,16 @@ export class AppComponent {
   tracksData = data;
   title = 'angular-spo';
   selectedSongs: Array<String> = [];
+
+  redirectURL() {
+    const url = new URL('https://accounts.spotify.com/authorize');
+    url.searchParams.set('client_id', environment.spotifyApiKey);
+    url.searchParams.set('response_type', 'token');
+    url.searchParams.set('scope', 'playlist-modify-private');
+    url.searchParams.set('redirect_uri', 'http://localhost:4200/');
+    return url.toString();
+  }
+
   selectEvent(uri: string) {
     if (this.selectedSongs.includes(uri)) {
       this.selectedSongs = this.selectedSongs.filter((el) => el !== uri);
@@ -17,5 +28,17 @@ export class AppComponent {
       this.selectedSongs = this.selectedSongs.concat([uri]);
     }
     console.log(this.selectedSongs);
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+    const parsedHash = new URLSearchParams(window.location.hash.substr(1));
+    if (parsedHash.has('access_token')) {
+      const accessToken: string = parsedHash.get('access_token') || '';
+      console.log(accessToken);
+      localStorage.setItem('token', accessToken);
+    }
   }
 }
